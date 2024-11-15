f27b2dabb85ccdaddc1b4072bc6e12ba2fa9113b
goldenknlg <snr7@outlook.com>
2024-11-15 10:33:35 -0500

    Working server need full http implamentation

---
+++ webserver.c
@@ -0,0 +1,90 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+	char buffer[BUFFER_SIZE];
+
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		perror("webserver (socket)");
+		return 1;
+	}
+	printf("socket created succesfully.\n");
+
+	struct sockaddr_in host_addr;
+	int host_addrlen = sizeof(host_addr);
+
+	host_addr.sin_family =AF_INET;
+	host_addr.sin_port = htons(PORT);
+	host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	struct sockaddr_in client_addr;
+	int client_addrlen = sizeof(client_addr);
+
+	if (bind(sockfd, (struct sockaddr *)&host_addr, host_addrlen) != 0) {
+		perror("webserver (bind)");
+		return 1;
+	}
+	printf("socket succesfully bound to [%d:%d].\n", INADDR_ANY, PORT);
+
+	if (listen(sockfd, SOMAXCONN) != 0) {
+		perror("webserver (listen)");
+		return 1;
+	}
+	printf("server listening for connections.\n");
+
+	for (;;) {
+		int newsockfd = accept(sockfd, (struct sockaddr *)&host_addr, (socklen_t *)&host_addrlen);
+		if (newsockfd < 0) {
+			perror("webserver (accept)");
+			continue;
+		}
+		printf("connection accepted\n");
+
+		int sockn = getsockname(newsockfd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addrlen);
+
+		if (sockn < 0) {
+			perror("webserver (getsockname)");
+			continue;
+		}
+
+		int valread = read(newsockfd, buffer, BUFFER_SIZE);
+		if (valread < 0) {
+			perror("webserver (read)");
+			continue;
+		}
+
+		char method[BUFFER_SIZE], uri[BUFFER_SIZE], version[BUFFER_SIZE];
+        sscanf(buffer, "%s %s %s", method,  uri, version);
+		printf("[%s:%u] %s %s %s\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), method, version, uri);
+        char resp[BUFFER_SIZE];
+
+        if (strcmp(method,"GET") == 0) {
+            strcpy(resp, 
+                "HTTP/1.0 200 OK\r\n"
+		        "Server: webserver-c\r\n"
+		        "Content-type: text/html\r\n\r\n"
+                );
+            strcat(resp, "<html> ");
+            strcat(resp, method);
+            strcat(resp, " </html>");
+        }
+		int valwrite = write(newsockfd, resp, strlen(resp));
+		if (valwrite < 0) {
+			perror("webserver (write)");
+			continue;
+		}
+
+		close(newsockfd);
+	}
+
+	return 0;
+}
